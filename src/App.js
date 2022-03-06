a3028822b9665d88327e18bbfbdf7552b14f0ae0
Jefferson Nascimento <jefferson.oliv@hotmail.com>
2022-03-06 19:02:37 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Questões e Perguntas sobre Login</h3>
+        <section className='info'>
+          {
+            questions.map((question) => (
+              <SingleQuestion key={question.id} {...question} />
+            ))
+          }
+
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
